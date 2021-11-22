91f826854b97948589aabfad1ed39acd1d747b7e
Err204 <err62906235@gmail.com>
2021-11-21 18:14:03 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ if("serviceWorker" in navigator){
     navigator.serviceWorker
         .register("./sw.js")
         .then(function(reg){
-            console.log("%cService Worker registered.", "color: green;", reg);
+            console.log("%cService Worker registered.", "color: limegreen;", reg);
         })
         .catch(function(err){
             console.error("Service Worker not registered.", err);
