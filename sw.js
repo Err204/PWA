19e58b0aaca7afeba5d77ead00ef31af090025e2
Err204 <err62906235@gmail.com>
2021-11-21 17:12:10 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,11 @@
-self.addEventListener("install", evt => {
+self.addEventListener("install", function(e){
     console.log("Service Worker has been installed.");
 });
 
-self.addEventListener("activate", evt => {
+self.addEventListener("activate", function(e){
     console.log("Service Worker has been activated.");
+});
+
+self.addEventListener("fetch", function(e){
+    console.log("Fetch event.", e);
 });
